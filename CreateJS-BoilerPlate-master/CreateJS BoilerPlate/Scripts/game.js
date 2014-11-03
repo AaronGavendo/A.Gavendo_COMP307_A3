01c54112070bdc1677b5ddebf575b51d20396b94
AaronGavendo <agavendo@hotmail.com>
2014-11-02 20:31:24 -0500

    #7 Add Enemy reset when hit by tank Add obstacle reset when hit by tank Add death and boom sound effects

--- CreateJS-BoilerPlate-master/CreateJS BoilerPlate/Scripts/game.js
+++ CreateJS-BoilerPlate-master/CreateJS BoilerPlate/Scripts/game.js
@@ -1,6 +1,7 @@
 ﻿var stage;
 var queue;
 
+var grass;
 var tank;
 var tnt = [];
 var nazis = [];
@@ -17,6 +18,7 @@ function preload() {
         { id: "tank", src: "images/Tank.png" },
         { id: "tnt", src: "images/TNT.png" },
         { id: "nazi1", src: "images/Nazi1.png" },
+        { id: "boom", src: "sounds/boom.wav" },
         { id: "death", src: "sounds/death.mp3" }
     ]);
 }
@@ -24,12 +26,15 @@ function preload() {
 function init() {
     stage = new createjs.Stage(document.getElementById("canvas"));
     stage.enableMouseOver(20);
+
+    //stage.cursor = "none"; If you want to hide Mouse
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", gameLoop);
     main();
 }
 
 function gameLoop(event) {
+    grass.update();
     tank.update();
     for (var t = 0; t < TNT_NUM; t++) {
         tnt[t].update();
@@ -37,6 +42,8 @@ function gameLoop(event) {
     for (var n = 0; n < NAZI_NUM; n++) {
         nazis[n].update();
     }
+
+    collision();
     stage.update();
 }
 
@@ -105,7 +112,92 @@ var TNT = (function () {
     return TNT;
 })();
 
+var Grass = (function () {
+    function Grass() {
+        this.image = new createjs.Bitmap(queue.getResult("grass"));
+        this.reset();
+        this.dy = 4; //Keep same as TNT to make look like its on ground
+        stage.addChild(this.image);
+    }
+    Grass.prototype.reset = function () {
+        this.image.x = -this.image.getBounds().width + stage.canvas.width;
+    };
+
+    Grass.prototype.update = function () {
+        this.image.x += this.dy;
+        if (this.image.x >= 0) {
+            this.reset();
+        }
+    };
+    return Grass;
+})();
+
+function tankAndTNT() {
+    for (var a = 0; a < TNT_NUM; a++) {
+        var p1;
+        var p2;
+
+        p1 = new createjs.Point();
+        p2 = new createjs.Point();
+
+        p1.x = tank.image.x;
+        p1.y = tank.image.y;
+        p2.x = tnt[a].image.x;
+        p2.y = tnt[a].image.y;
+
+        if (distance(p1, p2) < ((tank.image.getBounds().height / 2) + (tnt[a].image.getBounds().height / 2))) {
+            createjs.Sound.play("boom");
+            tnt[a].reset();
+            stage.update();
+        }
+    }
+}
+
+function tankAndNazi() {
+    for (var a = 0; a < NAZI_NUM; a++) {
+        var p1;
+        var p2;
+
+        p1 = new createjs.Point();
+        p2 = new createjs.Point();
+
+        p1.x = tank.image.x;
+        p1.y = tank.image.y;
+        p2.x = nazis[a].image.x;
+        p2.y = nazis[a].image.y;
+
+        if (distance(p1, p2) < ((tank.image.getBounds().height / 2) + (nazis[a].image.getBounds().height / 2))) {
+            createjs.Sound.play("death");
+            nazis[a].reset();
+            stage.update();
+        }
+    }
+}
+
+function collision() {
+    tankAndTNT();
+    tankAndNazi();
+}
+
+//Distance between two points
+function distance(p1, p2) {
+    var result = 0;
+    var xPoints = 0;
+    var yPoints = 0;
+
+    xPoints = p2.x - p1.x;
+    xPoints = xPoints * xPoints;
+
+    yPoints = p2.y - p1.y;
+    yPoints = yPoints * yPoints;
+
+    result = Math.sqrt(xPoints + yPoints);
+
+    return result;
+}
+
 function main() {
+    grass = new Grass();
     for (var i = 0; i < NAZI_NUM; i++) {
         nazis[i] = new Nazi();
     }
--- CreateJS-BoilerPlate-master/CreateJS BoilerPlate/Scripts/game.ts
+++ CreateJS-BoilerPlate-master/CreateJS BoilerPlate/Scripts/game.ts
@@ -1,6 +1,7 @@
 ﻿var stage: createjs.Stage;
 var queue;
 
+var grass: Grass
 var tank: Tank
 var tnt = [];
 var nazis = [];
@@ -17,6 +18,7 @@ function preload(): void {
         { id: "tank", src: "images/Tank.png" },
         { id: "tnt", src: "images/TNT.png" },
         { id: "nazi1", src: "images/Nazi1.png" },
+        { id: "boom", src: "sounds/boom.wav" },
         { id: "death", src: "sounds/death.mp3" }
     ]);
 }
@@ -24,12 +26,14 @@ function preload(): void {
 function init(): void {
     stage = new createjs.Stage(document.getElementById("canvas"));
     stage.enableMouseOver(20);
+    //stage.cursor = "none"; If you want to hide Mouse
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", gameLoop);
     main();
 }
 
 function gameLoop(event): void {
+    grass.update();
     tank.update();
     for (var t = 0; t < TNT_NUM; t++) {
         tnt[t].update();
@@ -37,6 +41,8 @@ function gameLoop(event): void {
     for (var n = 0; n < NAZI_NUM; n++) {
         nazis[n].update();
     }
+
+    collision();
     stage.update();
 }
 
@@ -113,10 +119,105 @@ class TNT {
     }
 }
 
+class Grass {
+    image: createjs.Bitmap;
+    dy: number;
+    constructor() {
+        this.image = new createjs.Bitmap(queue.getResult("grass"));
+        this.reset();
+        this.dy = 4; //Keep same as TNT to make look like its on ground
+        stage.addChild(this.image);
+    }
 
+    reset() {
+        this.image.x = -this.image.getBounds().width + stage.canvas.width;
+    }
+
+    update() {
+        this.image.x += this.dy;
+        if (this.image.x >= 0) {
+            this.reset();
+        }
+    }
+}
+
+function tankAndTNT()
+{
+    for (var a = 0; a < TNT_NUM; a++) {
+        var p1: createjs.Point;
+        var p2: createjs.Point;
+
+        p1 = new createjs.Point();
+        p2 = new createjs.Point();
+
+        p1.x = tank.image.x;
+        p1.y = tank.image.y;
+        p2.x = tnt[a].image.x;
+        p2.y = tnt[a].image.y;
+
+        if (distance(p1, p2) < ((tank.image.getBounds().height / 2) + (tnt[a].image.getBounds().height / 2))) {
+            createjs.Sound.play("boom");
+            tnt[a].reset();
+            stage.update();
+
+        }
+    }
+
+}
+
+function tankAndNazi() {
+    for (var a = 0; a < NAZI_NUM; a++) {
+        var p1: createjs.Point;
+        var p2: createjs.Point;
+
+        p1 = new createjs.Point();
+        p2 = new createjs.Point();
+
+        p1.x = tank.image.x;
+        p1.y = tank.image.y;
+        p2.x = nazis[a].image.x;
+        p2.y = nazis[a].image.y;
+
+        if (distance(p1, p2) < ((tank.image.getBounds().height / 2) + (nazis[a].image.getBounds().height / 2))) {
+            createjs.Sound.play("death");
+            nazis[a].reset();
+            stage.update();
+
+        }
+    }
+
+}
+
+function collision() {
+    tankAndTNT();
+    tankAndNazi();
+
+
+
+}
+
+
+//Distance between two points
+function distance(p1: createjs.Point, p2: createjs.Point): number {
+    var result: number = 0;
+    var xPoints: number = 0;
+    var yPoints: number = 0;
+
+    xPoints = p2.x - p1.x;
+    xPoints = xPoints * xPoints;
+
+    yPoints = p2.y - p1.y;
+    yPoints = yPoints * yPoints;
+
+    result = Math.sqrt(xPoints + yPoints);
+
+    return result;
+
+}
 
 function main(): void
 {
+    grass = new Grass();
     for (var i = 0; i < NAZI_NUM; i++) {
         nazis[i] = new Nazi();
     }
@@ -124,4 +225,5 @@ function main(): void
         tnt[o] = new TNT();
     }
     tank = new Tank();
+
 }
